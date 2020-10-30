2e25ce2556e3697d6c8f6a126383fdafe7b2223c
류명균 <ryu@tting-gyuns-maegbug.local>
2020-10-30 23:17:13 +0900

    flask-cors

--- app.py
+++ app.py
@@ -1,39 +1,34 @@
 from flask import Flask, render_template
 from flask import jsonify
+from flask_cors import CORS
 import threading
 import random
+
 
 # import predictor_s1, predictor2, predictor3 
 import predictor_s1, predictor_s2
 
 app = Flask(__name__)
+CORS(app)
 
 # just in case...
 @app.route('/')
-def api():
-    class_name_1, class_precision_1 = predictor_s1.deploy()
-    class_name_2, class_precision_2 = predictor_s2.deploy()
-    nop_1 = str(class_name_1)
-    pre_1 = str(class_precision_1)
-    nop_2 = str(class_name_2)
-    pre_2 = str(class_precision_2)
+def hello():
+    return "Hello World!"
 
-    sensor = {"nop_1": nop_1, "precision_1": pre_1,
-              "nop_2": nop_2, "precision_2": pre_2}
-    return jsonify(sensor)
-# @app.route('/dashboard1')
-# def dashboard1():
-#     # class name = NoP
-#     # class precision = precision
-#     class_name, class_precision = predictor_s1.deploy()
-#     return render_template('dashboard1.html', name=class_name, precision=class_precision)
+@app.route('/dashboard1')
+def dashboard1():
+    # class name = NoP
+    # class precision = precision
+    class_name, class_precision = predictor_s1.deploy()
+    return render_template('dashboard1.html', name=class_name, precision=class_precision)
 
-# @app.route('/dashboard2')
-# def dashboard2():
-#     # class name = NoP
-#     # class precision = precision
-#     class_name, class_precision = predictor_s2.deploy()
-#     return render_template('dashboard2.html', name=class_name, precision=class_precision)
+@app.route('/dashboard2')
+def dashboard2():
+    # class name = NoP
+    # class precision = precision
+    class_name, class_precision = predictor_s2.deploy()
+    return render_template('dashboard2.html', name=class_name, precision=class_precision)
 
 # @app.route('/dashboard3')
 # def dashboard3():
@@ -51,32 +46,32 @@ def api():
 ######json data for react ######
 
 
-# @app.route('/api')
-# def api():
-#     class_name_1, class_precision_1 = predictor_s1.deploy()
-#     class_name_2, class_precision_2 = predictor_s2.deploy()
-#     nop_1 = str(class_name_1)
-#     pre_1 = str(class_precision_1)
-#     nop_2 = str(class_name_2)
-#     pre_2 = str(class_precision_2)
+@app.route('/api')
+def api():
+    class_name_1, class_precision_1 = predictor_s1.deploy()
+    class_name_2, class_precision_2 = predictor_s2.deploy()
+    nop_1 = str(class_name_1)
+    pre_1 = str(class_precision_1)
+    nop_2 = str(class_name_2)
+    pre_2 = str(class_precision_2)
 
-#     sensor = {"nop_1": nop_1, "precision_1": pre_1,
-#               "nop_2": nop_2, "precision_2": pre_2}
-#     return jsonify(sensor)
+    sensor = [{"id" : "10", "nop": nop_1, "precision": pre_1},{"id" : "12", "nop": nop_2, "precision": pre_2}]
+
+    return jsonify(sensor)
 
 
-# @app.route('/api2')
-# def api2(methods=['GET']):
-#     class_name_1, class_precision_1 = predictor_s1.deploy()
-#     class_name_2, class_precision_2 = predictor_s2.deploy()
-#     nop_1 = str(class_name_1)
-#     pre_1 = str(class_precision_1)
-#     nop_2 = str(class_name_2)
-#     pre_2 = str(class_precision_2)
+@app.route('/api2')
+def api2(methods=['GET']):
+    class_name_1, class_precision_1 = predictor_s1.deploy()
+    class_name_2, class_precision_2 = predictor_s2.deploy()
+    nop_1 = str(class_name_1)
+    pre_1 = str(class_precision_1)
+    nop_2 = str(class_name_2)
+    pre_2 = str(class_precision_2)
 
-#     sensor = {"nop_1": nop_1, "precision_1": pre_1,
-#               "nop_2": nop_2, "precision_2": pre_2}
-#     return jsonify(sensor)
+    sensor = {"nop_1": nop_1, "precision_1": pre_1,
+              "nop_2": nop_2, "precision_2": pre_2}
+    return jsonify(sensor)
 
 # @app.route('/dashboard3')
 # def dashboard3():
@@ -92,4 +87,4 @@ def api():
 #     return render_template('index.html', name=class_name, result=result_name, precision=class_precision)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(ssl_context='adhoc')
